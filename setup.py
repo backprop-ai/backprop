1d0d98fd6a9b554e683dc61a769e2a09e788721a
Kristo Ojasaar <ojasaarkristo@gmail.com>
2020-11-01 14:44:11 +0200

    pypi setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kiri",  # Replace with your own username
+    version="0.0.1",
+    author="Kiri OÜ",
+    author_email="hello@kiri.ai",
+    description="Kiri Natural Language Engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kiri-ai/kiri",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
