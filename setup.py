ae93cab44dc7ed7d1db50dc30d032abebbd4e013
Kristo Ojasaar <ojasaarkristo@gmail.com>
2020-11-03 19:28:48 +0200

    Initial functional release to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kiri",  # Replace with your own username
-    version="0.0.1",
+    version="0.1.0",
     author="Kiri OÜ",
     author_email="hello@kiri.ai",
     description="Kiri Natural Language Engine",
