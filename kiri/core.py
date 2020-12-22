6cbdfb47cf8482e7cd9d8bfcdf8328dc5ebb3df3
Owen LaCava <owenlacava@gmail.com>
2020-12-22 18:07:12 +0200

    QA in core

--- kiri/core.py
+++ kiri/core.py
@@ -1,5 +1,6 @@
 from typing import Callable, Dict, List
 from sentence_transformers import SentenceTransformer
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 from .search import DocStore, SearchResults, Document
 from .utils import process_document, process_results
@@ -11,6 +12,7 @@ class Kiri:
     Attributes:
         store: DocStore object to be used as the engine backend
         vectorize_model: Name of the SentenceTransformer model to be used in operations
+        qa_model: Name of HuggingFace model to be used for Question/Answer
         process_doc_func: Function to be used when vectorizing updloaded documents
         process_results_func: Function to be used for calculating final scores of results
     
@@ -19,6 +21,7 @@ class Kiri:
     """
 
     def __init__(self, store: DocStore, vectorize_model: str = None,
+                 qa_model: str = None,
                  process_doc_func: Callable[[
                      Document, SentenceTransformer], List[float]] = None,
                  process_results_func: Callable[[SearchResults, SentenceTransformer], None] = None):
@@ -29,6 +32,11 @@ class Kiri:
         if vectorize_model is None:
             # Use default vectorization model
             vectorize_model = "ojasaar/distilbert-sentence-msmarco-en-et"
+
+        if qa_model is None:
+            # Default, may not even be T5 soon.
+            # TODO: Handle different Transformer types.
+            qa_model = "google/t5-small"
 
         if process_doc_func is None:
             # Use default vectorizer
@@ -41,6 +49,8 @@ class Kiri:
         self._process_doc_func = process_doc_func
         self._process_results_func = process_results
         self._vectorize_model = SentenceTransformer(vectorize_model)
+        self._qa_model = T5ForConditionalGeneration.from_pretrained(qa_model)
+        self._qa_tokenizer = T5Tokenizer.from_pretrained(qa_model)
 
     def upload(self, documents: List[Document]) -> None:
         """Uploads documents to store
@@ -72,3 +82,33 @@ class Kiri:
         self._process_results_func(
             search_results, query_vec, self._store._doc_class, preview_length)
         return search_results
+
+    # TODO: "Modularize" this -- make separate QA components,
+    #        rather than directly doing logic in here.
+    #        Need default handling function (T5) w/ this logic,
+    #        overridable with other function.
+    def qa(self, query: str, context: str = None, context_doc: Document = None):
+        """Perform QA, either on docstore or on provided context.
+
+        """
+        if context_doc or context:
+            c_string = context if context else context_doc.content
+            input_text = f"q: {query} c: {c_string}"
+            features = self._qa_tokenizer([input_text], return_tensors="pt")
+            output = self._qa_model(input_ids=features["input_ids"].cuda(),
+                                    attention_mask=features["attention_mask"].cuda())
+            answer = self._qa_tokenizer.decode(output[0])
+            return answer
+        else:
+            search_results = self.search(query)
+            answers = []
+            for result in search_results[:3]:
+                text = result.document.content
+                input_text = f"q: {query} c: {text}"
+                features = self._qa_tokenizer([input_text], return_tensors="pt")
+                output = self._qa_model(input_ids=features["input_ids"].cuda(),
+                                        attention_mask=features["attention_mask"].cuda())
+                answer = self._qa_tokenizer.decode(output[0])
+                answers.append(answer)
+            return zip(answers, search_results[:3])
+            
