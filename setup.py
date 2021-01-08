a3ac3f182aada5f22d7ca4a90bb6155a472cdb44
Kristo Ojasaar <ojasaarkristo@gmail.com>
2021-01-08 19:03:19 +0200

    Update to 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kiri",
-    version="0.2.1",
+    version="0.3.0",
     author="Kiri OÜ",
     author_email="hello@kiri.ai",
     description="Kiri Natural Language Engine",
