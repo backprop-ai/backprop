0b6643a82808318c280ddb76022db83f26498eac
Kristo Ojasaar <ojasaarkristo@gmail.com>
2021-01-20 15:38:25 +0200

    Switched to double quotes

--- kiri/models/b_generate.py
+++ kiri/models/b_generate.py
@@ -5,23 +5,23 @@ tokenizer = None
 m_type = None
 
 model_types = {
-    't5': T5ForConditionalGeneration,
-    'gpt2': GPT2LMHeadModel
+    "t5": T5ForConditionalGeneration,
+    "gpt2": GPT2LMHeadModel
 }
 
 tokenizer_types = {
-    't5': T5Tokenizer,
-    'gpt2': GPT2Tokenizer
+    "t5": T5Tokenizer,
+    "gpt2": GPT2Tokenizer
 }
 
 instance_names = {
-    't5': 't5-small',
-    'gpt2': 'gpt2'
+    "t5": "t5-small",
+    "gpt2": "gpt2"
 }
 
-def b_generate(input_text: str, model_type: str = 'gpt2',
+def b_generate(input_text: str, model_type: str = "gpt2",
                model_name: str = None, tokenizer_name: str = None,
-               local: bool = True, device: str = 'cpu'):
+               local: bool = True, device: str = "cpu"):
 
     global model
     global tokenizer
@@ -30,7 +30,7 @@ def b_generate(input_text: str, model_type: str = 'gpt2',
     if local:
 
         if model_type not in model_types.keys():
-            raise ValueError(f'Model type not supported! Supported types: {list(model_types.keys())}')
+            raise ValueError(f"Model type not supported! Supported types: {list(model_types.keys())}")
         
         if not model or model_type != m_type:
             if not model_name:
@@ -47,7 +47,7 @@ def b_generate(input_text: str, model_type: str = 'gpt2',
 
         m_type = model_type
 
-        encoded_input = tokenizer.encode(input_text, return_tensors='pt')
+        encoded_input = tokenizer.encode(input_text, return_tensors="pt")
         encoded_input = encoded_input.to(device)
 
         output = model.generate(input_ids=encoded_input, num_return_sequences=1)
@@ -55,7 +55,7 @@ def b_generate(input_text: str, model_type: str = 'gpt2',
         output = tokenizer.decode(output[0])
         return output
     else:
-        raise ValueError('Non-local inference is not currently implemented')
+        raise ValueError("Non-local inference is not currently implemented")
 
 
 
