9ab11fa969a6d95526805174f80c581b446da8ee
Kristo Ojasaar <ojasaarkristo@gmail.com>
2021-03-09 16:36:35 +0200

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kiri",
-    version="0.5.0.dev3",
+    version="0.5.0",
     author="Kiri OÜ",
     author_email="hello@kiri.ai",
     description="Kiri Natural Language Engine",
