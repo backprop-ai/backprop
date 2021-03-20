aa05ab7043b8ddc603f8b27c45a6b671cc73c7bb
Kristo Ojasaar <ojasaarkristo@gmail.com>
2021-03-20 16:06:18 +0200

    0.0.3 with efficientnet finetuning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="backprop",
-    version="0.0.2",
+    version="0.0.3",
     author="Backprop",
     author_email="hello@backprop.co",
     description="Backprop",
