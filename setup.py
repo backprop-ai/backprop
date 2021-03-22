c0d9c891b3ddbb7c8b534aa79041092d65cf17cc
Kristo Ojasaar <ojasaarkristo@gmail.com>
2021-03-22 15:55:07 +0200

    0.0.8 image-vectorisation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="backprop",
-    version="0.0.7",
+    version="0.0.8",
     author="Backprop",
     author_email="hello@backprop.co",
     description="Backprop",
