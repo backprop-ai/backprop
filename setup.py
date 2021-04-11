f5a50433c3498e770f6dbe47607eb132f4f77ed1
Kristo Ojasaar <ojasaarkristo@gmail.com>
2021-04-11 21:34:23 +0300

    0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="backprop",
-    version="0.1.0.dev1",
+    version="0.1.0",
     author="Backprop",
     author_email="hello@backprop.co",
     description="Backprop",
