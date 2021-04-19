bbf964a5ba4dc48e58b6d0dc53f95b4884130cba
Kristo <73708394+ojasaar@users.noreply.github.com>
2021-04-19 20:21:09 +0300

    Update conf.py
    correct docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Backprop'
 author = 'Backprop'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.9'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
